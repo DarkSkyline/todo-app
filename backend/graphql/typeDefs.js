c222b640497d265e7bfd257ccc0513a9527e6842
Filipe Ferreira <filipesferreira94@gmail.com>
2021-09-20 00:06:45 +0100

    feat: create some types

--- backend/graphql/typeDefs.js
+++ backend/graphql/typeDefs.js
@@ -2,13 +2,28 @@ const { gql } = require('apollo-server')
 
 module.exports = gql`
     type Task{
-        id: ID!,
-        title: String!,
-        body: String!,
-        username: String!,
+        id: ID!
+        title: String!
+        body: String!
+        username: String!
         createdAt: String!
+    }
+    type User{
+        id: ID!
+        username: String!
+        token: String!
+        createdAt: String!
+    }
+    input RegisterInput{
+        username: String!
+        password: String!
+        confirmPassword: String!
     }
     type Query{
         getTasks: [Task]
+    }
+    type Mutation{
+        register(registerInput: RegisterInput): User!
+        login(username: String!, password: String!)
     }
 `
