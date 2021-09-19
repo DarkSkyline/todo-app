1a967e823751a118514ac5355b22bc6f626406ce
Filipe Ferreira <filipesferreira94@gmail.com>
2021-09-19 23:23:30 +0100

    feat: refactor resolvers and typeDefs

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,9 @@
 const { ApolloServer } = require('apollo-server')
-const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 
-const Task = require('./models/Tasks')
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
 const { MONGODB } = require('./config.js')
-
-const typeDefs = gql`
-    type Task{
-        id: ID!,
-        title: String!,
-        body: String!,
-        username: String!,
-        createdAt: String!
-    }
-    type Query{
-        getTasks: [Task]
-    }
-`
-
-const resolvers = {
-    Query: {
-        async getTasks(){
-            try{
-                const posts = await Task.find();
-                return posts;
-            } catch(err) {
-                throw new Error(err)
-            }
-        }
-    }
-}
 
 const server = new ApolloServer({
     typeDefs,
