a8c0dc40bd84f66e19b86e532994716b94b30537
Filipe Ferreira <filipesferreira94@gmail.com>
2021-09-19 22:42:45 +0100

    feat: add a connection to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
 const mongoose = require('mongoose')
+const { MONGODB } = require('./config.js')
 
 const typeDefs = gql`
     type Query{
@@ -19,9 +20,11 @@ const server = new ApolloServer({
     resolvers
 })
 
-mongoose.connect()
-
-server.listen({ port: 5000 })
-.then(res => {
-    console.log(`Server running at ${res.url}` )
-})
+mongoose.connect(MONGODB, { useNewUrlParser: true})
+    .then(() => {
+        console.log('MongoDB connected')
+        return server.listen({ port: 5000})
+    })
+    .then(res => {
+        console.log(`Server running at ${res.url}` )
+    })
