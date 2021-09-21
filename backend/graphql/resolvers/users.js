7b1b12ae5b0aaf14a01010c14bc6cd119e4119b4
Filipe Ferreira <filipesferreira94@gmail.com>
2021-09-21 16:07:42 +0100

    feat: create login method and refactor some code

--- backend/graphql/resolvers/users.js
+++ backend/graphql/resolvers/users.js
@@ -6,8 +6,44 @@ const { validateRegisterInput, validateLoginInput } = require('../../utils/valid
 const { SECRET_KEY } = require('../../config')
 const User = require('../../models/Users')
 
+function generateToken(user){
+    return jwt.sign({
+        id: user.id,
+        username: user.username
+    }, SECRET_KEY, { expiresIn: '1h' })
+}
+
 module.exports = {
     Mutation: {
+        async login(_, { username, password }) {
+            const { errors, valid } = validateLoginInput(username, password)
+            
+            if(!valid){
+                throw new UserInputError('Errors', { errors })
+            }
+
+            const user = await User.findOne({ username })
+
+            if(!user) {
+                errors.general = 'User not found'
+                throw new UserInputError('Wrong credentials', { errors })
+            }
+
+            const match = await bcrypt.compare(password, user.password)
+            if(!match) {
+                errors.general = 'Wrong credentials'
+                throw new UserInputError('Wrong credentials', { errors })
+            }
+
+            const token = generateToken(user)
+
+            return {
+                ...user._doc,
+                id: user._id,
+                token
+            }
+        },
+
         async register(_, { registerInput : { username, password, confirmPassword }}, context, info){
             // Validate user data
             const { valid, errors } = validateRegisterInput(username, password, confirmPassword)
@@ -34,11 +70,8 @@ module.exports = {
             
             const res = await newUser.save()
 
-            const token = jwt.sign({
-                id: res.id,
-                username: res.username
-            }, SECRET_KEY, { expiresIn: '1h' })
-            
+            const token = generateToken(res)
+
             return {
                 ...res._doc,
                 id: res._id,
